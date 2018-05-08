0e299427b3446c8f02a6b947e13d1813d5dd7afd
OSO1995 <okunevso@inbox.ru>
2018-05-08 17:52:02 +0400

    db(work)

--- src/main/java/nc/projects/network/network/StartTest.java
+++ src/main/java/nc/projects/network/network/StartTest.java
@@ -4,23 +4,45 @@ import nc.projects.network.network.entity.Role;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 
 public class StartTest {
     public static void main(String[] args) {
-        SessionFactory sessionFactory =
-                new Configuration()
-                        .configure()
-                        .buildSessionFactory();
+        buildSessionFactory();
+        SessionFactory sessionFactory = getSessionFactory();
 
         Session session = sessionFactory.openSession();
 
-        Transaction transaction = session.beginTransaction();
-
         Role role = new Role();
-        role.setId(1L);
         role.setName("ROLE_USER");
 
         session.save(role);
+        shutdown();
     }
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new AnnotationConfiguration().configure().buildSessionFactory();
+
+        } catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        // Close caches and connection pools
+        getSessionFactory().close();
+    }
+
+
 }
