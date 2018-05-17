6380963a401e60bb35883db2b30be53b88eec816
OSO1995 <34703208+OSO1995@users.noreply.github.com>
2018-05-17 22:23:00 +0400

    4

--- src/main/java/nc/project/network/validator/UserValidator.java
+++ src/main/java/nc/project/network/validator/UserValidator.java
@@ -1,6 +1,6 @@
 package nc.project.network.validator;
 
-import nc.project.network.entity.UserBase;
+import nc.project.network.entity.User;
 import nc.project.network.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -16,28 +16,28 @@ public class UserValidator implements Validator {
 
     @Override
     public boolean supports(Class<?> aClass) {
-        return UserBase.class.equals(aClass);
+        return User.class.equals(aClass);
     }
 
     @Override
     public void validate(Object o, Errors errors) {
-        UserBase userBase = (UserBase) o;
+        User user = (User) o;
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "Required");
-        if (userBase.getUsername().length() < 8 || userBase.getUsername().length() > 32) {
+        if (user.getUsername().length() < 8 || user.getUsername().length() > 32) {
             errors.rejectValue("username", "Size.userForm.username");
         }
 
-        if (userService.findByUsername(userBase.getUsername()) != null) {
+        if (userService.findByUsername(user.getUsername()) != null) {
             errors.rejectValue("username", "Duplicate.userForm.username");
         }
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "Required");
-        if (userBase.getPassword().length() < 8 || userBase.getPassword().length() > 32) {
+        if (user.getPassword().length() < 8 || user.getPassword().length() > 32) {
             errors.rejectValue("password", "Size.userForm.password");
         }
 
-        if (!userBase.getConfirmPassword().equals(userBase.getPassword())) {
+        if (!user.getConfirmPassword().equals(user.getPassword())) {
             errors.rejectValue("confirmPassword", "Different.userForm.password");
         }
     }
